55282ec97f0cabb3ad79d9e89411979b2f423cc2
Morton Jonuschat <m.jonuschat@gute-botschafter.de>
2011-05-03 11:26:24 +0200

    Re-indent & whitespace

--- Classes/Domain/Model/Event.php
+++ Classes/Domain/Model/Event.php
@@ -1,167 +1,166 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2011 Morton Jonuschat <m.jonuschat@gute-botschafter.de>, Gute Botschafter GmbH
-*  
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 3 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2011 Morton Jonuschat <m.jonuschat@gute-botschafter.de>, Gute Botschafter GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * A single event
  */
 class Tx_GbEvents_Domain_Model_Event extends Tx_Extbase_DomainObject_AbstractEntity {
 
-	/**
-	 * The title of the event
-	 *
-	 * @var string
-	 * @validate NotEmpty
-	 */
-	protected $title;
+  /**
+   * The title of the event
+   *
+   * @var string
+   * @validate NotEmpty
+   */
+  protected $title;
 
-	/**
-	 * A short teaser text
-	 *
-	 * @var string
-	 */
-	protected $teaser;
+  /**
+   * A short teaser text
+   *
+   * @var string
+   */
+  protected $teaser;
 
-	/**
-	 * A detailed description of the event
-	 *
-	 * @var string
-	 * @validate NotEmpty
-	 */
-	protected $description;
+  /**
+   * A detailed description of the event
+   *
+   * @var string
+   * @validate NotEmpty
+   */
+  protected $description;
 
-	/**
-	 * The location of the event
-	 *
-	 * @var string
-	 */
-	protected $location;
+  /**
+   * The location of the event
+   *
+   * @var string
+   */
+  protected $location;
 
-	/**
-	 * The date when the event happens
-	 *
-	 * @var DateTime
-	 * @validate NotEmpty
-	 */
-	protected $eventDate;
+  /**
+   * The date when the event happens
+   *
+   * @var DateTime
+   * @validate NotEmpty
+   */
+  protected $eventDate;
 
-	/**
-	 * The time the event happens
-	 *
-	 * @var string
-	 */
-	protected $eventTime;
+  /**
+   * The time the event happens
+   *
+   * @var string
+   */
+  protected $eventTime;
 
-	/**
-	 * @param string $title
-	 * @return void
-	 */
-	public function setTitle($title) {
-		$this->title = $title;
-	}
+  /**
+   * @param string $title
+   * @return void
+   */
+  public function setTitle($title) {
+    $this->title = $title;
+  }
 
-	/**
-	 * @return string
-	 */
-	public function getTitle() {
-		return $this->title;
-	}
+  /**
+   * @return string
+   */
+  public function getTitle() {
+    return $this->title;
+  }
 
-	/**
-	 * @param string $teaser
-	 * @return void
-	 */
-	public function setTeaser($teaser) {
-		$this->teaser = $teaser;
-	}
+  /**
+   * @param string $teaser
+   * @return void
+   */
+  public function setTeaser($teaser) {
+    $this->teaser = $teaser;
+  }
 
-	/**
-	 * @return string
-	 */
-	public function getTeaser() {
-		return $this->teaser;
-	}
+  /**
+   * @return string
+   */
+  public function getTeaser() {
+    return $this->teaser;
+  }
 
-	/**
-	 * @param string $description
-	 * @return void
-	 */
-	public function setDescription($description) {
-		$this->description = $description;
-	}
+  /**
+   * @param string $description
+   * @return void
+   */
+  public function setDescription($description) {
+    $this->description = $description;
+  }
 
-	/**
-	 * @return string
-	 */
-	public function getDescription() {
-		return $this->description;
-	}
+  /**
+   * @return string
+   */
+  public function getDescription() {
+    return $this->description;
+  }
 
-	/**
-	 * @param string $location
-	 * @return void
-	 */
-	public function setLocation($location) {
-		$this->location = $location;
-	}
+  /**
+   * @param string $location
+   * @return void
+   */
+  public function setLocation($location) {
+    $this->location = $location;
+  }
 
-	/**
-	 * @return string
-	 */
-	public function getLocation() {
-		return $this->location;
-	}
+  /**
+   * @return string
+   */
+  public function getLocation() {
+    return $this->location;
+  }
 
-	/**
-	 * @param DateTime $eventDate
-	 * @return void
-	 */
-	public function setEventDate(DateTime $eventDate) {
-		$this->eventDate = $eventDate;
-	}
+  /**
+   * @param DateTime $eventDate
+   * @return void
+   */
+  public function setEventDate(DateTime $eventDate) {
+    $this->eventDate = $eventDate;
+  }
 
-	/**
-	 * @return DateTime
-	 */
-	public function getEventDate() {
-		return $this->eventDate;
-	}
+  /**
+   * @return DateTime
+   */
+  public function getEventDate() {
+    return $this->eventDate;
+  }
 
-	/**
-	 * @param string $eventTime
-	 * @return void
-	 */
-	public function setEventTime($eventTime) {
-		$this->eventTime = $eventTime;
-	}
+  /**
+   * @param string $eventTime
+   * @return void
+   */
+  public function setEventTime($eventTime) {
+    $this->eventTime = $eventTime;
+  }
 
-	/**
-	 * @return string
-	 */
-	public function getEventTime() {
-		return $this->eventTime;
-	}
+  /**
+   * @return string
+   */
+  public function getEventTime() {
+    return $this->eventTime;
+  }
 
 }
-?>
--- Tests/Domain/Model/EventTest.php
+++ Tests/Domain/Model/EventTest.php
@@ -1,28 +1,28 @@
 <?php
 
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2011 Morton Jonuschat <m.jonuschat@gute-botschafter.de>, Gute Botschafter GmbH
-*  			
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2011 Morton Jonuschat <m.jonuschat@gute-botschafter.de>, Gute Botschafter GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * Testcase for class Tx_GbEvents_Domain_Model_Event.
@@ -33,118 +33,118 @@
  *
  * @package TYPO3
  * @subpackage Terminkalender
- * 
+ *
  * @author Morton Jonuschat <m.jonuschat@gute-botschafter.de>
  */
 class Tx_GbEvents_Domain_Model_EventTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
-	/**
-	 * @var Tx_GbEvents_Domain_Model_Event
-	 */
-	protected $fixture;
+  /**
+   * @var Tx_GbEvents_Domain_Model_Event
+   */
+  protected $fixture;
 
-	public function setUp() {
-		$this->fixture = new Tx_GbEvents_Domain_Model_Event();
-	}
+  public function setUp() {
+    $this->fixture = new Tx_GbEvents_Domain_Model_Event();
+  }
 
-	public function tearDown() {
-		unset($this->fixture);
-	}
-	
-	
-	/**
-	 * @test
-	 */
-	public function getTitleReturnsInitialValueForString() { }
+  public function tearDown() {
+    unset($this->fixture);
+  }
 
-	/**
-	 * @test
-	 */
-	public function setTitleForStringSetsTitle() { 
-		$this->fixture->setTitle('Conceived at T3CON10');
 
-		$this->assertSame(
-			'Conceived at T3CON10',
-			$this->fixture->getTitle()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function getTeaserReturnsInitialValueForString() { }
+  /**
+   * @test
+   */
+  public function getTitleReturnsInitialValueForString() { }
 
-	/**
-	 * @test
-	 */
-	public function setTeaserForStringSetsTeaser() { 
-		$this->fixture->setTeaser('Conceived at T3CON10');
+    /**
+     * @test
+     */
+    public function setTitleForStringSetsTitle() {
+      $this->fixture->setTitle('Conceived at T3CON10');
 
-		$this->assertSame(
-			'Conceived at T3CON10',
-			$this->fixture->getTeaser()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function getDescriptionReturnsInitialValueForString() { }
+      $this->assertSame(
+        'Conceived at T3CON10',
+        $this->fixture->getTitle()
+      );
+    }
 
-	/**
-	 * @test
-	 */
-	public function setDescriptionForStringSetsDescription() { 
-		$this->fixture->setDescription('Conceived at T3CON10');
+  /**
+   * @test
+   */
+  public function getTeaserReturnsInitialValueForString() { }
 
-		$this->assertSame(
-			'Conceived at T3CON10',
-			$this->fixture->getDescription()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function getLocationReturnsInitialValueForString() { }
+    /**
+     * @test
+     */
+    public function setTeaserForStringSetsTeaser() {
+      $this->fixture->setTeaser('Conceived at T3CON10');
 
-	/**
-	 * @test
-	 */
-	public function setLocationForStringSetsLocation() { 
-		$this->fixture->setLocation('Conceived at T3CON10');
+      $this->assertSame(
+        'Conceived at T3CON10',
+        $this->fixture->getTeaser()
+      );
+    }
 
-		$this->assertSame(
-			'Conceived at T3CON10',
-			$this->fixture->getLocation()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function getEventDateReturnsInitialValueForDateTime() { }
+  /**
+   * @test
+   */
+  public function getDescriptionReturnsInitialValueForString() { }
 
-	/**
-	 * @test
-	 */
-	public function setEventDateForDateTimeSetsEventDate() { }
-	
-	/**
-	 * @test
-	 */
-	public function getEventTimeReturnsInitialValueForString() { }
+    /**
+     * @test
+     */
+    public function setDescriptionForStringSetsDescription() {
+      $this->fixture->setDescription('Conceived at T3CON10');
 
-	/**
-	 * @test
-	 */
-	public function setEventTimeForStringSetsEventTime() { 
-		$this->fixture->setEventTime('Conceived at T3CON10');
+      $this->assertSame(
+        'Conceived at T3CON10',
+        $this->fixture->getDescription()
+      );
+    }
 
-		$this->assertSame(
-			'Conceived at T3CON10',
-			$this->fixture->getEventTime()
-		);
-	}
-	
+  /**
+   * @test
+   */
+  public function getLocationReturnsInitialValueForString() { }
+
+    /**
+     * @test
+     */
+    public function setLocationForStringSetsLocation() {
+      $this->fixture->setLocation('Conceived at T3CON10');
+
+      $this->assertSame(
+        'Conceived at T3CON10',
+        $this->fixture->getLocation()
+      );
+    }
+
+  /**
+   * @test
+   */
+  public function getEventDateReturnsInitialValueForDateTime() { }
+
+    /**
+     * @test
+     */
+    public function setEventDateForDateTimeSetsEventDate() { }
+
+    /**
+     * @test
+     */
+    public function getEventTimeReturnsInitialValueForString() { }
+
+    /**
+     * @test
+     */
+    public function setEventTimeForStringSetsEventTime() {
+      $this->fixture->setEventTime('Conceived at T3CON10');
+
+      $this->assertSame(
+        'Conceived at T3CON10',
+        $this->fixture->getEventTime()
+      );
+    }
+
 }
 ?>
