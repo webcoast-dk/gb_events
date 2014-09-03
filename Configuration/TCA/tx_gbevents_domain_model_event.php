7bd78fa7dea11b6b1c2647feebac736e4d448b7f
Morton Jonuschat <m.jonuschat@gute-botschafter.de>
2014-09-03 14:04:32 +0200

    Recommended TCA setup for TYPO3 6.x

--- Configuration/TCA/tx_gbevents_domain_model_event.php
+++ Configuration/TCA/tx_gbevents_domain_model_event.php
@@ -1,19 +1,42 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-  die ('Access denied.');
+if (!defined('TYPO3_MODE')) {
+  die('Access denied.');
 }
 
-$TCA['tx_gbevents_domain_model_event'] = array(
-  'ctrl' => $TCA['tx_gbevents_domain_model_event']['ctrl'],
+return array(
+  'ctrl' => array(
+    'title' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event',
+    'label' => 'title',
+    'tstamp' => 'tstamp',
+    'crdate' => 'crdate',
+    'dividers2tabs' => TRUE,
+    'versioningWS' => 2,
+    'versioning_followPages' => TRUE,
+    'origUid' => 't3_origuid',
+    'languageField' => 'sys_language_uid',
+    'transOrigPointerField' => 'l10n_parent',
+    'transOrigDiffSourceField' => 'l10n_diffsource',
+    'delete' => 'deleted',
+    'enablecolumns' => array(
+      'disabled' => 'hidden',
+      'starttime' => 'starttime',
+      'endtime' => 'endtime',
+    ) ,
+    'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('gb_events') . 'Resources/Public/Icons/tx_gbevents_domain_model_event.gif'
+  ) ,
   'interface' => array(
     'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, title, teaser, description, location, event_date, event_time, event_stop_date, images, downloads, recurring_weeks, recurring_days, recurring_stop, recurring_exclude_holidays, recurring_exclude_dates',
-  ),
+  ) ,
   'types' => array(
-    '1' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, title, teaser, description;;;richtext[]:rte_transform[mode=ts_css|imgpath=uploads/tx_gbevents/rte/], location, event_date, event_time, event_stop_date, images, downloads,--div--;LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring, recurring_weeks, recurring_days, recurring_stop, recurring_exclude_holidays, recurring_exclude_dates,--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,starttime, endtime'),
-  ),
+    '1' => array(
+      'showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, title, teaser, description;;;richtext[]:rte_transform[mode=ts_css|imgpath=uploads/tx_gbevents/rte/], location, event_date, event_time, event_stop_date, images, downloads,--div--;LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring, recurring_weeks, recurring_days, recurring_stop, recurring_exclude_holidays, recurring_exclude_dates,--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,starttime, endtime'
+    ) ,
+  ) ,
   'palettes' => array(
-    '1' => array('showitem' => ''),
-  ),
+    '1' => array(
+      'showitem' => ''
+    ) ,
+  ) ,
   'columns' => array(
     'sys_language_uid' => array(
       'exclude' => 1,
@@ -23,11 +46,16 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'foreign_table' => 'sys_language',
         'foreign_table_where' => 'ORDER BY sys_language.title',
         'items' => array(
-          array('LLL:EXT:lang/locallang_general.php:LGL.allLanguages', -1),
-          array('LLL:EXT:lang/locallang_general.php:LGL.default_value', 0)
-        ),
-      ),
-    ),
+          array(
+            'LLL:EXT:lang/locallang_general.php:LGL.allLanguages', -1
+          ) ,
+          array(
+            'LLL:EXT:lang/locallang_general.php:LGL.default_value',
+            0
+          )
+        ) ,
+      ) ,
+    ) ,
     'l10n_parent' => array(
       'displayCond' => 'FIELD:sys_language_uid:>:0',
       'exclude' => 1,
@@ -35,32 +63,35 @@ $TCA['tx_gbevents_domain_model_event'] = array(
       'config' => array(
         'type' => 'select',
         'items' => array(
-          array('', 0),
-        ),
+          array(
+            '',
+            0
+          ) ,
+        ) ,
         'foreign_table' => 'tx_gbevents_domain_model_event',
         'foreign_table_where' => 'AND tx_gbevents_domain_model_event.pid=###CURRENT_PID### AND tx_gbevents_domain_model_event.sys_language_uid IN (-1,0)',
-      ),
-    ),
+      ) ,
+    ) ,
     'l10n_diffsource' => array(
-      'config' =>array(
-        'type' =>'passthrough',
-      ),
-    ),
+      'config' => array(
+        'type' => 'passthrough',
+      ) ,
+    ) ,
     't3ver_label' => array(
       'displayCond' => 'FIELD:t3ver_label:REQ:true',
       'label' => 'LLL:EXT:lang/locallang_general.php:LGL.versionLabel',
       'config' => array(
-        'type' =>'none',
+        'type' => 'none',
         'cols' => 27,
-      ),
-    ),
+      ) ,
+    ) ,
     'hidden' => array(
       'exclude' => 1,
       'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.hidden',
       'config' => array(
         'type' => 'check',
-      ),
-    ),
+      ) ,
+    ) ,
     'starttime' => array(
       'exclude' => 1,
       'l10n_mode' => 'mergeIfNotBlank',
@@ -72,8 +103,8 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'eval' => 'datetime',
         'checkbox' => '0',
         'default' => '0',
-      ),
-    ),
+      ) ,
+    ) ,
     'endtime' => array(
       'exclude' => 1,
       'l10n_mode' => 'mergeIfNotBlank',
@@ -86,11 +117,11 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'checkbox' => '0',
         'default' => '0',
         'range' => array(
-          'upper' => mktime(0, 0, 0, 12, 31, date('Y') + 10),
-          'lower' => mktime(0, 0, 0, date('m')-1, date('d'), date('Y'))
-        ),
-      ),
-    ),
+          'upper' => mktime(0, 0, 0, 12, 31, date('Y') + 10) ,
+          'lower' => mktime(0, 0, 0, date('m') - 1, date('d') , date('Y'))
+        ) ,
+      ) ,
+    ) ,
     'title' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.title',
@@ -98,8 +129,8 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'type' => 'input',
         'size' => 30,
         'eval' => 'trim,required'
-      ),
-    ),
+      ) ,
+    ) ,
     'teaser' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.teaser',
@@ -108,8 +139,8 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'cols' => 40,
         'rows' => 15,
         'eval' => 'trim'
-      ),
-    ),
+      ) ,
+    ) ,
     'description' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.description',
@@ -118,8 +149,8 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'cols' => 40,
         'rows' => 15,
         'eval' => 'trim,required'
-      ),
-    ),
+      ) ,
+    ) ,
     'location' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.location',
@@ -127,8 +158,8 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'type' => 'input',
         'size' => 30,
         'eval' => 'trim'
-      ),
-    ),
+      ) ,
+    ) ,
     'event_date' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.event_date',
@@ -138,8 +169,8 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'max' => 20,
         'eval' => 'date,required',
         'checkbox' => 1
-      ),
-    ),
+      ) ,
+    ) ,
     'event_time' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.event_time',
@@ -147,8 +178,8 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'type' => 'input',
         'size' => 30,
         'eval' => 'trim'
-      ),
-    ),
+      ) ,
+    ) ,
     'event_stop_date' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.event_stop_date',
@@ -158,41 +189,80 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'max' => 20,
         'eval' => 'date',
         'checkbox' => 1
-      ),
-    ),
+      ) ,
+    ) ,
     'recurring_weeks' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks',
       'config' => array(
         'type' => 'check',
         'items' => array(
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.0', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.1', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.2', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.3', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.4', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.5', ''),
-        ),
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.0',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.1',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.2',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.3',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.4',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_weeks.5',
+            ''
+          ) ,
+        ) ,
         'suppress_icons' => 1,
-      ),
-    ),
+      ) ,
+    ) ,
     'recurring_days' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days',
       'config' => array(
         'type' => 'check',
         'items' => array(
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.0', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.1', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.2', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.3', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.4', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.5', ''),
-          array('LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.6', ''),
-        ),
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.0',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.1',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.2',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.3',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.4',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.5',
+            ''
+          ) ,
+          array(
+            'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_days.6',
+            ''
+          ) ,
+        ) ,
         'suppress_icons' => 1,
-      ),
-    ),
+      ) ,
+    ) ,
     'recurring_stop' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_stop',
@@ -202,16 +272,16 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'max' => 20,
         'eval' => 'datetime',
         'checkbox' => 1,
-      ),
-    ),
+      ) ,
+    ) ,
     'recurring_exclude_holidays' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_exclude_holidays',
       'config' => array(
         'type' => 'check',
         'default' => 0,
-      ),
-    ),
+      ) ,
+    ) ,
     'recurring_exclude_dates' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.recurring_exclude_dates',
@@ -220,12 +290,12 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'cols' => 40,
         'rows' => 15,
         'eval' => 'trim'
-      ),
-    ),
+      ) ,
+    ) ,
     'images' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.images',
-      'config' => array (
+      'config' => array(
         'type' => 'group',
         'internal_type' => 'file',
         'allowed' => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
@@ -235,12 +305,12 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'size' => 5,
         'minitems' => 0,
         'maxitems' => 5,
-      ),
-    ),
+      ) ,
+    ) ,
     'downloads' => array(
       'exclude' => 0,
       'label' => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event.downloads',
-      'config' => array (
+      'config' => array(
         'type' => 'group',
         'internal_type' => 'file',
         'allowed' => '',
@@ -251,7 +321,7 @@ $TCA['tx_gbevents_domain_model_event'] = array(
         'size' => 5,
         'minitems' => 0,
         'maxitems' => 5,
-      ),
-    ),
-  ),
+      ) ,
+    ) ,
+  ) ,
 );
--- ext_tables.php
+++ ext_tables.php
@@ -18,26 +18,3 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist']['gbevents_main'] = '
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_gbevents_domain_model_event', 'EXT:gb_events/Resources/Private/Language/locallang_csh_tx_gbevents_domain_model_event.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_gbevents_domain_model_event');
-$TCA['tx_gbevents_domain_model_event'] = array(
-  'ctrl' => array(
-    'title'                     => 'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents_domain_model_event',
-    'label'                     => 'title',
-    'tstamp'                    => 'tstamp',
-    'crdate'                    => 'crdate',
-    'dividers2tabs'             => TRUE,
-    'versioningWS'              => 2,
-    'versioning_followPages'    => TRUE,
-    'origUid'                   => 't3_origuid',
-    'languageField'             => 'sys_language_uid',
-    'transOrigPointerField'     => 'l10n_parent',
-    'transOrigDiffSourceField'  => 'l10n_diffsource',
-    'delete'                    => 'deleted',
-    'enablecolumns'             => array(
-      'disabled'  => 'hidden',
-      'starttime' => 'starttime',
-      'endtime'   => 'endtime',
-    ),
-    'dynamicConfigFile'         => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Event.php',
-    'iconfile'                  => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_gbevents_domain_model_event.gif'
-  ),
-);
