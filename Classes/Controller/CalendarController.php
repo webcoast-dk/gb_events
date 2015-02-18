4506b2ee2b20d972c25b2373f981672bebc9e2cd
Morton Jonuschat <m.jonuschat@gute-botschafter.de>
2015-02-18 12:14:47 +0100

    Extract calendar view into plugin

---
+++ Classes/Controller/CalendarController.php
@@ -0,0 +1,107 @@
+<?php
+namespace GuteBotschafter\GbEvents\Controller;
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011-2013 Morton Jonuschat <m.jonuschat@gute-botschafter.de>, Gute Botschafter GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Controller for the calendar view
+ */
+class CalendarController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+
+  /**
+   * @var \GuteBotschafter\GbEvents\Domain\Repository\EventRepository
+   * @inject
+   */
+  protected $eventRepository;
+
+
+  /**
+   * Displays all events as a browseable calendar
+   *
+   * @param  string $start
+   * @return void
+   */
+  public function showAction($start = 'today') {
+    // Startdatum setzen
+    $startDate = new \DateTime('today');
+    try {
+      $startDate->modify($start);
+    } catch(Exception $e) {
+      $startDate->modify('midnight');
+    }
+
+    // Start für Kalenderanzeige bestimmen
+    $preDate = clone($startDate);
+    if($startDate->format('N') !== 1) {
+      $preDate->modify('last monday of previous month');
+    }
+
+    // Ende des Monats bestimmen
+    $stopDate = clone($startDate);
+    $stopDate->modify('last day of this month');
+    $stopDate->modify('+86399 seconds');
+
+    $postDate = clone($stopDate);
+    if($stopDate->format('N') !== 7) {
+      $postDate->modify('next sunday');
+    }
+
+    // Navigational dates
+    $nextMonth = clone($startDate);
+    $nextMonth->modify('first day of next month');
+    $previousMonth = clone($startDate);
+    $previousMonth->modify('first day of previous month');
+
+    $days = array();
+    $runDate = clone($preDate);
+    while($runDate <= $postDate) {
+      $days[$runDate->format('Y-m-d')] = array('date' => clone($runDate), 'events' => array(), 'disabled' => (($runDate < $startDate) || ($runDate > $stopDate)));
+      $runDate->modify('tomorrow');
+    }
+
+    $events = $this->eventRepository->findAllBetween($preDate, $postDate);
+    foreach($events as $eventDay => $eventsThisDay) {
+      $days[$eventDay]['events'] = $eventsThisDay['events'];
+    }
+
+    $weeks = array();
+    $visibleWeeks = floor(count($days)/7);
+    for($i = 0; $i < $visibleWeeks; $i++) {
+      $weeks[] = array_slice($days, $i*7, 7, TRUE);
+    }
+
+    $this->view->assignMultiple(array(
+      'calendar' => $weeks,
+      'navigation' => array(
+        'previous' => $previousMonth,
+        'current' => $startDate,
+        'next' => $nextMonth
+      ),
+      'nextMonth' => $nextMonth->format('Y-m-d'),
+      'prevMonth' => $previousMonth->format('Y-m-d')
+    ));
+  }
+}
--- ext_tables.php
+++ ext_tables.php
@@ -25,6 +25,17 @@ $TCA['tt_content']['types']['list']['subtypes_addlist']['gbevents_upcoming'] = '
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('gbevents_upcoming', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/Upcoming.xml');
 $TCA['tt_content']['types']['list']['subtypes_excludelist']['gbevents_upcoming'] = 'layout,select_key,recursive';
 
+# Calendar Plugin (Calendar view of events)
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+  'GuteBotschafter.' . $_EXTKEY,
+  'Calendar',
+  'LLL:EXT:gb_events/Resources/Private/Language/locallang_db.xlf:tx_gbevents.calendar.title'
+);
+
+$TCA['tt_content']['types']['list']['subtypes_addlist']['gbevents_calendar'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('gbevents_calendar', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/Calendar.xml');
+$TCA['tt_content']['types']['list']['subtypes_excludelist']['gbevents_calendar'] = 'layout,select_key,recursive';
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Terminkalender');
 
 
