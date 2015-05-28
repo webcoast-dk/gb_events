e301de4e9d84e7275bd1a51a2a65f07c8b95cdeb
Morton Jonuschat <m.jonuschat@gute-botschafter.de>
2015-05-28 09:40:49 +0200

    [TASK] Integrate FAL Migration Wizard into Install Tool

--- ext_localconf.php
+++ ext_localconf.php
@@ -36,3 +36,6 @@ if(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('ke_search')) {
   $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerIndexerConfiguration'][] = 'EXT:gb_events/Classes/Hooks/EventIndexer.php:GuteBotschafter\GbEvents\Hooks\EventIndexer';
   $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['customIndexer'][] = 'GuteBotschafter\GbEvents\Hooks\EventIndexer';
 }
+
+// Update scripts
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['gbevents_fal'] = 'GuteBotschafter\\GbEvents\\Updates\\FalUpdateWizard';
