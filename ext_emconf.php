2fa4a0ab4e73ecb9c88555dca988f25c70bef70c
Thorben Nissen <thorben@webcoast.dk>
2020-11-15 21:18:43 +0100

    [BUGFIX] Wrong TYPO3 version in `ext_emconf.php`

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-7.0.99',
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '8.5.0-9.5.99',
         ],
     ],
     'autoload' => [
